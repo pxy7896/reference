dbb5fe651c215198c4d3ed30e22bce435a077dc5
Ruriko Pang <2419817128@qq.com>
2019-03-12 16:03:03 +0800

    Create cplusplus_tutorial.cpp

---
+++ cplusplus_tutorial.cpp
@@ -0,0 +1,7 @@
+Trigraphs
+The ASCII special characters [, ], {, }, |, and \ occupy character set positions designated as alphabetic by ISO. In most European national ISO-646 character sets, 
+these positions are occupied by letters not found in the English alphabet. Then use trigraphs.
+Trigraph	Replacement
+??= #
+??/	\
+??'	^
